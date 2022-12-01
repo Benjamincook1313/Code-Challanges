eaab71f10ceec4933e5b08e3d8d068a72e43410b
Benjamin Cook <benjamincook1313@gmail.com>
2022-12-01 16:02:15 -0700

    Final

--- BallClock.js
+++ BallClock.js
@@ -13,12 +13,14 @@ function ballClock(num){
   let run = true;
   const queue = [...origin];
   
+  // rolls balls back into the queue in reverse order
   const rollBack = (arr) => {
     while(arr.length > 0){
       queue.push(arr.pop())
     }
   }
 
+  // checks if arrays are back in the same order
   const isEqual = (arr1, arr2) => {
     for(let i=0; i<arr1.length; i++){
       if(arr1[i] !== arr2[i]) return false;
@@ -34,30 +36,21 @@ function ballClock(num){
     // adds 5th ball to m5
     if(min1.length === 5) {
       min5.push(min1.pop());
-      // console.log("min1", min1);
       rollBack(min1.splice(0, 4));
-      // console.log("queue", queue, min5);
-      // run = false;
     }
 
     // moves m5 back to queue in reverse
     // add 12th ball to hr
     if(min5.length === 12){
       hr.push(min5.pop());
-      // console.log("min5", min5);
       rollBack(min5.splice(0, 11));
-      // console.log("queue", queue);
-      // run = false;
     }
 
     // moves hr back to queue in reverse order
     if(hr.length === 12){
-      // console.log(hr);
       rollBack(hr.splice(0, 11));
       queue.push(hr.pop())
-      // console.log(queue);
       count += .5;
-      // run = false;
     }
 
     run = !isEqual(origin, queue);
